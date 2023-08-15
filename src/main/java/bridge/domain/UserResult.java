bb4d3e6e3c12470aa9eafb8954dbecc0c77799f6
chosunghyun18 <krismon@naver.com>
2023-08-15 14:30:03 +0900

    feat : add outView print message -게임의 진행 상황및 결과를 출력을 담당 하는 역할

---
+++ src/main/java/bridge/domain/UserResult.java
@@ -0,0 +1,23 @@
+package bridge.domain;
+
+import java.util.List;
+
+public class UserResult {
+
+    private List<String> upBridge;
+    private List<String> downBridge;
+    private String gameClear ;
+    private int gameTry ;
+    public String getGameClear() {
+        return gameClear;
+    }
+    public int getGameTry() {
+        return getGameTry();
+    }
+    public List<String> getUpBridge() {
+        return  upBridge ;
+    }
+    public List<String> getDownBridge() {
+        return downBridge ;
+    }
+}
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,31 @@
 package bridge.view;
 
+import bridge.domain.UserResult;
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
-
+    public void printStartInfoMessage() {
+        System.out.println("다리 건너기 게임을 시작합니다.\n");
+    }
+    public void printBridgeSizeInfoMessage() {
+        System.out.println("다리의 길이를 입력해주세요.\n");
+    }
+    public void printMoveInfoMessage() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+    public void printRestartInputInfoMessage() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> up ,List<String> down ) {
+
     }
 
     /**
@@ -18,6 +33,10 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(UserResult userResult) {
+        System.out.println("최종 게임 결과");
+        printMap(userResult.getUpBridge(),userResult.getDownBridge()) ;
+        System.out.println("게임 성공 여부: "+userResult.getGameClear());
+        System.out.println("총 시도한 횟수: "+userResult.getGameTry());
     }
 }

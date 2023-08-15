53434bf7b1b88032441822bd90a2c5765fe69913
chosunghyun18 <krismon@naver.com>
2023-08-15 20:29:42 +0900

    refact : move  errorMessage view to controller

--- src/main/java/bridge/controller/InputController.java
+++ src/main/java/bridge/controller/InputController.java
@@ -3,41 +3,47 @@ package bridge.controller;
 import bridge.view.InputView;
 
 public class InputController {
+
+    private static final String ERROR_MESSAGE = "[ERROR]";
     private final InputView inputView;
     private final OutputController outputController;
+
     public InputController() {
+        this.inputView = new InputView();
         this.outputController = new OutputController();
-        this.inputView  =  new InputView();
     }
+
+    private void printErrorMessage(String e) {
+        System.out.println(ERROR_MESSAGE + e);
+    }
+
     public int readBridgeSize() {
         try {
             return inputView.readBridgeSize();
-        }catch (IllegalArgumentException e){
-            System.out.println(e);
+        } catch (IllegalArgumentException e) {
+            printErrorMessage(e.getMessage());
             outputController.printBridgeSizeInfoMessage();
             return readBridgeSize();
         }
     }
 
     public String readMoving() {
-        try{
+        try {
             return inputView.readMoving();
-        }catch (IllegalArgumentException e) {
-            System.out.println(e);
+        } catch (IllegalArgumentException e) {
+            printErrorMessage(e.getMessage());
             outputController.printMoveInfoMessage();
-        return readMoving();
+            return readMoving();
         }
     }
 
-
     public String readGameCommand() {
-        try{
+        try {
             return inputView.readGameCommand();
-        }catch (IllegalArgumentException e) {
-            System.out.println(e);
+        } catch (IllegalArgumentException e) {
+            printErrorMessage(e.getMessage());
             outputController.printRestartInputInfoMessage();
             return readGameCommand();
         }
     }
-
 }
--- src/main/java/bridge/validate/InputValidate.java
+++ src/main/java/bridge/validate/InputValidate.java
@@ -5,32 +5,41 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputValidate {
-    private static String ERROR_ME = "[ERROR]";
+
     public int convertSingleInt(String input) {
         try {
             int givenInt = Integer.parseInt(input);
-            if( givenInt > 20) throw new IllegalArgumentException(ERROR_ME +"over 20");
-            if( givenInt < 3 ) throw new IllegalArgumentException(ERROR_ME +"under 3");
-            return givenInt ;
-        }catch (NumberFormatException e){
-            throw new IllegalArgumentException(ERROR_ME+": not number");
+            if (givenInt > 20) throw new IllegalArgumentException("over 20");
+            if (givenInt < 3) throw new IllegalArgumentException("under 3");
+            return givenInt;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("not number");
         }
     }
 
     public void sizeInputCheckForBridgeSize(String input) {
-        if( input.length() > 2 ) throw new IllegalArgumentException( ERROR_ME + "over size");
+        if (input.length() > 2) {
+            throw new IllegalArgumentException("over size");
+        }
     }
-    public void readMovingCheck(String input) {
-        List<String> canRead = new ArrayList<>(Arrays.asList("U","D"));
-        lengthAndContainCommandCheck(input,canRead);
+
+    public void readMovingCheck(String move) {
+        List<String> canRead = new ArrayList<>(Arrays.asList("U", "D"));
+        lengthAndContainCommandCheck(move, canRead);
     }
-    public void readGameRestartCommand(String input) {
-        List<String> canRead = new ArrayList<>(Arrays.asList("R","Q"));
-        lengthAndContainCommandCheck(input,canRead);
+
+    public void readGameRestartCommand(String command) {
+        List<String> canRead = new ArrayList<>(Arrays.asList("R", "Q"));
+        lengthAndContainCommandCheck(command, canRead);
     }
-    private void lengthAndContainCommandCheck(String command,List<String> accept){
-        if(command.length() != 1) throw new IllegalArgumentException(ERROR_ME+"over size");
-        if(!accept.contains(command))throw new IllegalArgumentException(ERROR_ME+"wrong move");
+
+    private void lengthAndContainCommandCheck(String command, List<String> accept) {
+        if (command.length() != 1) {
+            throw new IllegalArgumentException("over size");
+        }
+        if (!accept.contains(command)) {
+            throw new IllegalArgumentException("wrong move");
+        }
     }
 
 }

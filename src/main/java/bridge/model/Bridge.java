143d21e7441ab0f243d1ff45c47ea49436c00a03
Brian <bwook9908@gmail.com>
2023-08-16 15:21:39 +0900

    feat: Bridge Class 구현
    - 다리를 객체로 저장
    - 입력값을 받았을 떄, 제대로 건넌건지 판단하는 로직

---
+++ src/main/java/bridge/model/Bridge.java
@@ -0,0 +1,18 @@
+package bridge.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bridge {
+    private final List<String> bridge;
+
+    public Bridge(List<String> bridge) {
+        this.bridge = bridge;
+    }
+
+    public boolean isMovementCorrect(String movement, int location) {
+        ArrayList<String> bridgeCheck = new ArrayList<>(bridge);
+        return bridgeCheck.get(location).equals(movement);
+    }
+
+}

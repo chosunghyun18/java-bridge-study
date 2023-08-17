336ac1644c83be87d9e87bf1d81767f48d9f719c
Ryuhyis <skyblue22756@gmail.com>
2023-08-17 10:59:07 +0900

    feat: outputView 기능 구현

---
+++ src/main/java/bridge/view/OutputView.java
@@ -0,0 +1,62 @@
+package bridge.view;
+
+import bridge.domain.UserBridge;
+
+import java.util.List;
+
+/**
+ * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ */
+public class OutputView {
+    public void printStartGameMessage() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+        System.out.println();
+    }
+
+    public void printRetryOrExit() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        System.out.println();
+    }
+
+    /**
+     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printMap(UserBridge userBridge) {
+        List<String> upBridge = userBridge.getUpBridge();
+        List<String> downBridge = userBridge.getDownBridge();
+        System.out.print("[ ");
+        for (int i = 0; i < upBridge.size(); i++) {
+            System.out.print(upBridge.get(i));
+            if (i != upBridge.size() - 1) {
+                System.out.print(" | ");
+            }
+        }
+        System.out.println(" ]");
+
+        System.out.print("[ ");
+        for (int i = 0; i < downBridge.size(); i++) {
+            System.out.print(downBridge.get(i));
+            if (i != downBridge.size() - 1) {
+                System.out.print(" | ");
+            }
+        }
+        System.out.println(" ]");
+        System.out.println();
+    }
+
+    /**
+     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+     * <p>
+     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     */
+    public void printResult(UserBridge userBridge, int attempts) {
+        System.out.println("최종 게임 결과");
+        printMap(userBridge);
+        boolean isSuccess = !userBridge.getUpBridge().contains("X") && !userBridge.getDownBridge().contains("X");
+        System.out.println("게임 성공 여부: " + (isSuccess ? "성공" : "실패"));
+        System.out.println("총 시도한 횟수: " + attempts);
+//        System.out.println("총 시도한 횟수: " + userBridge.getCount());
+    }
+}
